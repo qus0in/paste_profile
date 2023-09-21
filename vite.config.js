fe459ce7c16654cd2901b933e2b4f612dfc2c256
qus0in <qus0in@gmail.com>
2023-09-22 06:31:02 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "./paste_profile",
+  publicDir: "/paste_profile/",
   plugins: [
     vue(),
   ],
